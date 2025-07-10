12c64799201f72d0e727d0fb20a2982442367c26
CourseConsultants <nlhchin711@gmail.com>
2025-07-10 11:26:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ import numpy as np
 nInst = 50
 currentPos = np.zeros(nInst)
 
+import numpy as np
+
+nInst = 50
+currentPos = np.zeros(nInst)
+
 def getMyPosition(prcSoFar):
     global currentPos
-    (nins, nt) = prcSoFar.shape
-    if (nt < 2):
-        return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
+    n, t = prcSoFar.shape
+
+    if t < 20:
+        return np.zeros(n)
+
+    short_window = 5
+    long_window = 20
+
+    short_ma = np.mean(prcSoFar[:, -short_window:], axis=1)
+    long_ma = np.mean(prcSoFar[:, -long_window:], axis=1)
+
+    signals = np.sign(short_ma - long_ma)  # +1 = long, -1 = short
+
+    position_scale = 5000
+    prices = prcSoFar[:, -1]
+    max_units = 10000 // prices  # instrument limit
+
+    raw_pos = (signals * position_scale) // prices
+    clipped_pos = np.clip(raw_pos, -max_units, max_units)
+
+    currentPos = clipped_pos.astype(int)
     return currentPos
